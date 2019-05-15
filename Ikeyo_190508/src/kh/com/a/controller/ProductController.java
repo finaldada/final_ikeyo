542246951c56232c12e34d324ed181302186f331
dada <dabin227@naver.com>
2019-05-15 10:46:24 +0900

    3. inventory - insert

---
+++ Ikeyo_190508/src/kh/com/a/controller/ProductController.java
@@ -0,0 +1,209 @@
+package kh.com.a.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import kh.com.a.model.CartDto;
+import kh.com.a.model.InventoryDto;
+import kh.com.a.model.PagingParam;
+import kh.com.a.model.ProductDto;
+import kh.com.a.service.InventoryService;
+import kh.com.a.service.ProductService;
+import kh.com.a.util.FUpUtil;
+
+@Controller
+public class ProductController {
+	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+	
+	@Autowired		// 스프링프레임워크를 통해서 자동 생성  Service생성-> 
+	InventoryService inventoryService;
+	
+	@Autowired
+	ProductService productService;
+	
+	@RequestMapping(value = "productList.do", method = RequestMethod.GET)
+	public String productList(Model model, String category, PagingParam param) {
+		logger.info("ProductController productList " + new Date());
+		
+		int sn = param.getPageNumber();
+		int start = (sn) * param.getRecordCountPerPage()+1;
+		int end = (sn+1) * param.getRecordCountPerPage();
+		
+		param.setStart(start);
+		param.setEnd(end);
+		param.setCategory(category);
+		
+		List<ProductDto> productList = productService.getProductPagingList(param);
+		int totalRecordCount = productService.getProductCount(param);
+				//productService.getProductList(category);
+		List<CartDto> cartList = productService.getCartList(category);
+		
+		model.addAttribute("productList", productList);
+		model.addAttribute("pageNumber", sn);
+		model.addAttribute("pageCountPerScreen", 8);
+		model.addAttribute("recordCountPerPage", param.getRecordCountPerPage());
+		model.addAttribute("totalRecordCount", totalRecordCount);
+		model.addAttribute("category", param.getCategory());
+		model.addAttribute("cartList", cartList);
+		
+		return "productList.tiles";
+	}	
+	
+	@ResponseBody
+	@RequestMapping(value = "updateWish.do", method = RequestMethod.POST)
+	public Map<String, Object> updatewish(String model_id, String id, Model model) throws Exception {
+		logger.info("ProductController updateWish " + new Date());
+		
+		CartDto cart = new CartDto();
+		cart.setModel_id(model_id);
+		cart.setId(id);
+		
+		int cnt = productService.checkWish(cart);
+		int wishCnt = 0;
+		boolean isExist = false;
+		String result = "";
+		Map<String, Object> map = new HashMap<>();
+		
+		if(cnt == 0){
+			productService.updateCount(model_id);
+			wishCnt = productService.selectCount(model_id);
+			result = productService.updateWish(cart);
+			isExist = true;
+			map.put("result", result);
+			map.put("isExist", isExist);
+			map.put("wishCnt", wishCnt);
+		} else {
+			productService.deleteCount(model_id);
+			wishCnt = productService.selectCount(model_id);
+			result = productService.deleteWish(cart);
+			map.put("result", result);
+			map.put("isExist", isExist);
+			map.put("wishCnt", wishCnt);
+		}
+		return map;
+	}
+	
+	// 상품등록페이지 가기
+		@RequestMapping(value = "productAdd.do", method = {RequestMethod.GET, RequestMethod.POST})
+		public String ProductAdd(Model model) {
+
+			// 재고 리스트 받아오기
+			List<InventoryDto> invenlist = inventoryService.getInventoryList();
+			System.out.println("dddd:");
+			model.addAttribute("invenlist", invenlist);	// 모델에 재고리스트 날려줌
+
+			return "productAdd.tiles";
+		}
+
+		// 상품등록(파일업로드)
+		@RequestMapping(value = "productAddAf.do", method =RequestMethod.POST)
+		public String ProductWrite(ProductDto productDto,
+				@RequestParam(value="pdf_bs", required = false)MultipartFile pdf_bf,
+				@RequestParam(value="photo_bs1", required = false)MultipartFile photo_bf1,
+				@RequestParam(value="photo_bs2", required = false)MultipartFile photo_bf2,
+				@RequestParam(value="photo_bs3", required = false)MultipartFile photo_bf3) throws Exception {
+			
+			// filename을 취득
+			String filename = pdf_bf.getOriginalFilename();
+			String filename1 = photo_bf1.getOriginalFilename();
+			String filename2 = photo_bf2.getOriginalFilename();
+			String filename3 = photo_bf3.getOriginalFilename();
+
+			productDto.setPdf_bf(filename);
+			productDto.setPhoto_bf1(filename1);
+			productDto.setPhoto_bf2(filename2);
+			productDto.setPhoto_bf3(filename3);
+
+			// upload 경로
+			// tomcat
+			// String fupload = req.getServletContext().getRealPath("/upload");
+
+			// file
+			String fupload = "c:\\final_file";
+			
+			try {
+				
+				if(pdf_bf != null) {
+					// 파일명.xxx -> 12221321.xxx
+					String f = productDto.getPdf_bf();
+					String newfilename = FUpUtil.getNewFile(f);	
+					productDto.setPdf_af(newfilename);
+					File file = new File(fupload + "/" + newfilename);	// 파일을 생성	
+					// 실제 파일 업로드 부분 
+					FileUtils.writeByteArrayToFile(file, pdf_bf.getBytes());
+					
+					System.out.println("upload 파일경로 : " + fupload + "/" + newfilename);
+				}
+				if(photo_bf1 != null) {
+					// 파일명.xxx -> 12221321.xxx
+					String f = productDto.getPhoto_bf1();
+					String newfilename = FUpUtil.getNewFile(f);
+					productDto.setPhoto_af1(newfilename);
+					File file1 = new File(fupload + "/" + newfilename);	// 파일을 생성
+					// 실제 파일 업로드 부분 
+					FileUtils.writeByteArrayToFile(file1, photo_bf1.getBytes());
+				}
+				if(photo_bf2 != null) {
+					// 파일명.xxx -> 12221321.xxx
+					String f = productDto.getPhoto_bf2();
+					String newfilename = FUpUtil.getNewFile(f);
+					productDto.setPhoto_af2(newfilename);
+					File file2 = new File(fupload + "/" + newfilename);	// 파일을 생성
+					// 실제 파일 업로드 부분 
+					FileUtils.writeByteArrayToFile(file2, photo_bf2.getBytes());
+				}
+				if(photo_bf3 != null) {
+					// 파일명.xxx -> 12221321.xxx
+					String f = productDto.getPhoto_bf3();
+					String newfilename = FUpUtil.getNewFile(f);
+					productDto.setPhoto_af3(newfilename);
+					File file3 = new File(fupload + "/" + newfilename);	// 파일을 생성
+					// 실제 파일 업로드 부분 
+					FileUtils.writeByteArrayToFile(file3, photo_bf3.getBytes());
+				}
+
+				// DB에 저장
+				productService.productAdd(productDto);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return "redirect:/productList.do";
+		}
+
+		// 재고관리가져오는 ajax
+		@ResponseBody
+		@RequestMapping(value="getinventory.do", method= {RequestMethod.GET, RequestMethod.POST})
+		public Map<String, Object> productinput(int inven_seq) {
+			//System.out.println("inven_seq : " + inven_seq);
+			Map<String, Object> invenmap = new HashMap<String, Object>();
+
+			InventoryDto inven = inventoryService.getInventory(inven_seq);
+			//System.out.println("inven.model_id : " + inven.getModel_id());
+			invenmap.put("model_id", inven.getModel_id()+"");
+			invenmap.put("price", inven.getPrice());
+			invenmap.put("category", inven.getCategory()+"");
+			invenmap.put("count", inven.getCount());
+
+			return invenmap;
+		}
+}
---
+++ Ikeyo_190508/src/kh/com/a/service/InventoryService.java
@@ -0,0 +1,21 @@
+package kh.com.a.service;
+
+import java.util.List;
+
+import kh.com.a.model.BbsParam;
+import kh.com.a.model.InventoryDto;
+
+public interface InventoryService {
+
+	public List<InventoryDto> getInventoryList();
+	public List<InventoryDto> getInventoryPagingList(BbsParam param);
+	public int getInventoryCount(BbsParam param);
+	
+	public boolean inventoryWrite(InventoryDto inven);
+	public InventoryDto inventoryDetail(InventoryDto inven);
+	public String inventoryCheck(String model_id);
+	public boolean inventoryUpdate(InventoryDto inven);
+	public boolean inventoryDelete(InventoryDto inven);
+	
+	public InventoryDto getInventory(int inven_seq);
+}
